9ef2cbc0c4ec3128568f36788673d52ccbf67c93
frank1820 <franciscogonzalez_87@hotmail.com>
2018-02-20 16:24:19 +0100

    La conexion a la base de datos utiliza ahora un pool

--- src/Consulta.java
+++ src/Consulta.java
@@ -3,17 +3,20 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
 
 
 
@@ -21,6 +24,31 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/consulta")
 public class Consulta extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	DataSource pool;
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		
+		try {
+			InitialContext contexto= new InitialContext();
+			
+			 pool=(DataSource)contexto.lookup("java:comp/env/jdbc/mysql_tiendalibros");
+			
+			if (pool==null) {
+				throw new ServletException("ERROR AL ACCEDER AL POOL DE CONEXIONES: POOL");
+				
+			}
+			
+			
+		} catch (NamingException e) {
+			System.out.println("ERROR AL ACCEDER AL POOL DE CONEXIONES");
+			e.printStackTrace();
+		}
+		
+	}
        
 
     public Consulta() {
@@ -50,14 +78,15 @@ public class Consulta extends HttpServlet {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//Credenciales para la base de datos
-			String url="jdbc:mysql://localhost/TiendaLibros";
-			
-			String usuario="librero";
-			String password="Ageofempires2";
+//			String url="jdbc:mysql://localhost/TiendaLibros";
+//			
+//			String usuario="librero";
+//			String password="Ageofempires2";
 			
 			//Ejecutamos una consulta SQL.
+			con= pool.getConnection();
 			
-			con=DriverManager.getConnection(url, usuario, password);
+			//con=DriverManager.getConnection(url, usuario, password);
 			
 			String sql="Select * from libros where autor =?";
 			stmt=con.prepareStatement(sql);
